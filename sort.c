9ebaa7fb3b1a0eccc87675b200c131b8377c37f6
Sonya Statirova <44211670+sonya-statirova@users.noreply.github.com>
2018-11-17 00:10:24 +0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 //----------------------functions'_names-----------------------------------
 
-int OpenFileForRead(FILE** file, char* file_name);
+int InitializateAndOpenFileForRead(FILE** file, char* file_name);
 
-int OpenFileForWrite(FILE** file, char* file_name);
+int InitializateAndOpenFileForWrite(FILE** file, char* file_name);
 
 unsigned long FileLength(FILE* file);
 
-unsigned long CountStr(FILE* file);
+unsigned long CountStr(char* buf, unsigned long length);
 
 int PutBeginingsIndexes(char* buf, unsigned long length, char** beginings_of_str, unsigned long amount_of_str);
 
-int Rotater(char* buf, unsigned long length, char** beginings_of_str, unsigned long amount_of_str);
+int DeleteR_PutO(char* buf, unsigned long length);
+
+int Comparer(unsigned long str1, unsigned long str2, char** beginings_of_str, char* buf);
+
+int Sort(unsigned long amount_of_str, char** beginings_of_str, char* buf);
 
 int CreateNewBuf(char* buf, unsigned long length, char** beginings_of_str, unsigned long amount_of_str, char* bufnew);
 
@@ -27,125 +30,107 @@ int main()
     FILE* input_file = NULL;
     FILE* output_file = NULL;
 
-    if (OpenFileForRead(&input_file, input_file_name) == -1)
-    {
-        printf("Cannot open input file\n");
+    if (InitializateAndOpenFileForRead(&input_file, input_file_name) == -1)
+        {
+            printf("Cannot open input file\n");
 
-        return -1;
-    }
+            return -1;
+        }
 
-    if (OpenFileForWrite(&output_file, output_file_name) == -1)
-    {
-        printf("Cannot open output file\n");
-        fclose(input_file);
+    if (InitializateAndOpenFileForWrite(&output_file, output_file_name) == -1)
+        {
+            printf("Cannot open output file\n");
 
-        return -1;
-    }
+            return -1;
+        }
 
     unsigned long length = FileLength(input_file);
 
     if (length == -1)
-    {
-        printf("Incorrect file pointer\n");
+        {
+            printf("Incorrect file pointer\n");
 
-        return -1;
-    }
-
-    printf("str 54\n");
+            return -1;
+        }
 
     char* buf = (char*) calloc(length + 1, sizeof(char));
 
     if (buf == NULL)
-    {
-        printf("Cannot allocate memory for buf\n");
-        return -1;
-    }
+        {
+            printf("Cannot allocate memory for buf\n");
+            fclose(input_file);
+            fclose(output_file);
 
-    printf("str 65\n");
+            return -1;
+        }
 
     fseek(input_file, 0, SEEK_SET);
-    length = fread(buf, sizeof(char), length + 1, input_file);
-    printf("file real length = %lu\n", length);
+    fread(buf, sizeof(char), length + 1, input_file);
     fseek(input_file, 0, SEEK_SET);
 
-    printf("str 73\n");
+    unsigned long amount_of_str = CountStr(buf, length);
 
-    unsigned long amount_of_str = CountStr(input_file);
-    printf("number of str = %lu\n", amount_of_str);
     if (amount_of_str == -1)
-    {
-        printf("Incorrect file pointer\n");
+        {
+            printf("Incorrect file pointer\n");
 
-        return -1;
-    }
-
-    printf("str 83\n");
+            return -1;
+        }
 
     char** beginings_of_str = (char**) calloc(amount_of_str, sizeof(char*));
 
     if (beginings_of_str == NULL)
-    {
-        printf("Cannot allocate memory for beginings_of_str\n");
-        fclose(input_file);
-        fclose(output_file);
-        free(buf);
+        {
+            printf("Cannot allocate memory for beginings_of_str\n");
+            fclose(input_file);
+            fclose(output_file);
+            free(buf);
 
-        return -1;
-    }
-
-    printf("str 97\n");
+            return -1;
+        }
 
     if (PutBeginingsIndexes(buf, length, beginings_of_str, amount_of_str) == -1)
-    {
-        printf("Incorrect array pointer\n");
-        fclose(input_file);
-        fclose(output_file);
+        {
+            printf("Incorrect array pointer\n");
 
-        return -1;
-    }
+            return -1;
+        }
 
-    printf("str 108\n");
+    if (DeleteR_PutO( buf, length) == -1)
+        {
+            printf("Incorrect array pointer\n");
+
+            return -1;
+        }
 
     char* bufnew = (char*) calloc(length, sizeof(char));
 
     if (bufnew == NULL)
-    {
-        printf("Cannot allocate memory for bufnew\n");
-        fclose(input_file);
-        fclose(output_file);
-        free(buf);
-        free(beginings_of_str);
+        {
+            printf("Cannot allocate memory for bufnew\n");
+            fclose(input_file);
+            fclose(output_file);
+            free(buf);
+            free(beginings_of_str);
 
-        return -1;
-    }
+            return -1;
+        }
 
-    printf("str 123\n");
+    if (Sort(amount_of_str, beginings_of_str, buf) == -1)
+        {
+            printf("Incorrect array pointer or incorrect working of function Comparer\n");
 
-    if (Rotater(buf, length, beginings_of_str, amount_of_str) == -1)
-    {
-        printf("Incorrect array pointer\n");
-        fclose(input_file);
-        fclose(output_file);
-
-        return -1;
-    }
-
-    printf("str 134\n");
+            return -1;
+        }
 
     if (CreateNewBuf(buf, length, beginings_of_str, amount_of_str, bufnew) == -1)
-    {
-        printf("Incorrect array pointer\n");
-        fclose(input_file);
-        fclose(output_file);
+        {
+            printf("Incorrect array pointer\n");
 
-        return -1;
-    }
+            return -1;
+        }
 
-    printf("str 145\n");
-
-    fseek(input_file, 0, SEEK_SET);
     fwrite(bufnew, sizeof(char), length, output_file);
-    fseek(input_file, 0, SEEK_SET);
 
     fclose(input_file);
     fclose(output_file);
@@ -160,10 +145,12 @@ int main()
 
 //----------------------functions-----------------------------------
 
-int OpenFileForRead(FILE** file, char* file_name)
+int InitializateAndOpenFileForRead(FILE** file, char* file_name)
 {
     printf("Please, enter adress of file for reading in format *.txt\n");
-    scanf("%s", file_name);
+
+    if (scanf("%s", file_name) != 1)
+      return -1;
 
     *file = fopen(file_name, "r");
 
@@ -175,10 +162,12 @@ int OpenFileForRead(FILE** file, char* file_name)
 
 //----------------------------------------------------------------
 
-int OpenFileForWrite(FILE** file, char* file_name)
+int InitializateAndOpenFileForWrite(FILE** file, char* file_name)
 {
     printf("Please, enter adress of file for writing in format *.txt\n");
-    scanf("%s", file_name);
+
+    if (scanf("%s", file_name) != 1)
+      return -1;
 
     *file = fopen(file_name, "w");
 
@@ -199,41 +188,24 @@ unsigned long FileLength(FILE* file) /*length without EOF*/
     while (getc(file) != EOF)
       length++;
 
-  	/*fseek(file, 0, SEEK_END);
-
-    length = (ftell(file) + 1);*/
-
-    printf("length of file (expected without eof) = %lu\n", length);
     fseek(file, 0, SEEK_SET);
 
     return (length + 1);
 }
 
-//------------------------------------------------------------------
+//-------------------------------------------------------------------
 
-unsigned long CountStr(FILE* file)
+unsigned long CountStr(char* buf, unsigned long length)
 {
-    if (file == NULL)
-        return -1;
+	unsigned long count_str = 1;
 
-    fseek(file, 0, SEEK_SET);
+	for (unsigned long i = 0; i < length - 1; i++)
+	{
+		if (buf[i] == '\n')
+			count_str++;
+	}
 
-    unsigned long count_str = 0;
-
-    while (getc(file) != EOF)
-    {
-        fscanf(file, "%*[^\nEOF]");
-        count_str++;
-    }
-
-    fseek(file, -1, SEEK_END);
-
-    if (getc(file) == '\n')
-      count_str--;
-
-    fseek(file, 0, SEEK_SET);
-
-	return count_str;
+  return count_str;
 }
 
 //-----------------------------------------------------------------
@@ -256,55 +228,84 @@ int PutBeginingsIndexes(char* buf, unsigned long length, char** beginings_of_str
         if (buf[i] == '\n')
         {
             beginings_of_str[k] = &buf[i + 1];
-            buf[i] = '\0';
             k++;
         }
     }
 
-    if (buf[i] == '\n')
-      buf[i] = '\0';
-
     return 0;
+}
+
+//------------------------------------------------------------------
+
+int DeleteR_PutO(char* buf, unsigned long length)
+{
+  if (buf == NULL)
+      return -1;
+
+  for (unsigned long i = 0; i < (length); i++)
+  {
+      if (buf[i] == '\n')
+          buf[i] = '\0';
+  }
+
+  return 0;
 }
 
 //----------------------------------------------------------------
 
-int Rotater(char* buf, unsigned long length, char** beginings_of_str, unsigned long amount_of_str)
+int Comparer(unsigned long str1, unsigned long str2, char** beginings_of_str, char* buf)
 {
-  printf("am i alive?");
-    if (buf == NULL)
+  if (buf == NULL)
+      return -1;
+
+  if (beginings_of_str == NULL)
+      return -1;
+
+	for (unsigned long i = 0; *(beginings_of_str[str1] + i) != '\0' && *(beginings_of_str[str2] + i) != '\0'; i++)
+	{
+		if (*(beginings_of_str[str1] + i) < *(beginings_of_str[str2] + i))
+			return 1; //str1 should be before str2
+		else if (*(beginings_of_str[str1] + i) > *(beginings_of_str[str2] + i))
+				return 2; //str1 should be after str2
+	}
+	return 0; //str1 and str2 aare same
+}
+
+//-----------------------------------------------------------------
+
+int Sort(unsigned long amount_of_str, char** beginings_of_str, char* buf)
+{
+  if (buf == NULL)
+      return -1;
+
+  if (beginings_of_str == NULL)
+      return -1;
+
+	unsigned long i = 0;
+	char* k = 0;
+
+  int compare_result = 0;
+
+	for (unsigned long j = 0; j < amount_of_str - 1; j++)
+	{
+		i = 0;
+		while (i < amount_of_str - 1)
+		{
+      compare_result = Comparer(i, i + 1, beginings_of_str, buf);
+			if (compare_result == 2)
+			{
+				k = beginings_of_str[i];
+				beginings_of_str[i] = beginings_of_str[i + 1];
+				beginings_of_str[i + 1] = k;
+			}
+      else if (compare_result == -1)
         return -1;
 
-    if (beginings_of_str == NULL)
-        return -1;
+			i++;
+		}
+	}
 
-    char* rotate = 0;
-
-    for (unsigned long j = 1; j < amount_of_str; j++)
-    {
-        for (unsigned long i = 1; i < amount_of_str; i++)
-        {
-            for (unsigned long k = 0; beginings_of_str[i] + k < &buf[length] && beginings_of_str[i - 1] + k < &buf[length] && *(beginings_of_str[i - 1] + k) != '\0' && *(beginings_of_str[i] + k) != '\0'; k++)
-            {
-              assert(beginings_of_str[i] != NULL);
-              assert(beginings_of_str[i - 1] != NULL);
-              printf("%c", *(beginings_of_str[i] + k));
-                if (*(beginings_of_str[i - 1] + k) < *(beginings_of_str[i] + k))
-                {
-                    break;
-                }
-                else if (*(beginings_of_str[i - 1] + k) > *(beginings_of_str[i] + k))
-                {
-                    rotate = beginings_of_str[i];
-                    beginings_of_str[i] = beginings_of_str[i - 1];
-                    beginings_of_str[i - 1] = rotate;
-                    break;
-                }
-            }
-        }
-    }
-
-    return 0;
+  return 0;
 }
 
 //-----------------------------------------------------------------
